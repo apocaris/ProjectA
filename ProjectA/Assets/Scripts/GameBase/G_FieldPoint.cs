5cc0b8b66df67ec0f94be0f28df0c82058889381
apocaris <apocaris@naver.com>
2023-07-09 17:05:03 +0900

    v1.0.0 - 이동 및 스폰 가능 영역 지정
    Signed-off-by: apocaris <apocaris@naver.com>

--- ProjectA/Assets/Scripts/GameBase/G_FieldPoint.cs
+++ ProjectA/Assets/Scripts/GameBase/G_FieldPoint.cs
@@ -11,6 +11,29 @@ public class G_FieldPoint : G_Object
     public Transform a_vSpawnLimitMin { get { return m_vSpawnLimitMin; } }
     public Transform a_vSpawnLimitMax { get { return m_vSpawnLimitMax; } }
 
+    private void OnDrawGizmos()
+    {
+        if (m_vSpawnLimitMin != null && m_vSpawnLimitMax != null)
+        {
+            Gizmos.color = Color.yellow;
+            if (m_vSpawnLimitMin != null && m_vSpawnLimitMax != null)
+            {
+                float fWidth = m_vSpawnLimitMax.transform.position.x - m_vSpawnLimitMin.transform.position.x;
+                float fHeight = m_vSpawnLimitMax.transform.position.y - m_vSpawnLimitMin.transform.position.y;
+                m_vLimitArea = new Vector3(fWidth, fHeight, 0);
+
+                float fCenterX = m_vSpawnLimitMin.transform.position.x + (fWidth / 2);
+                float fCenterY = m_vSpawnLimitMin.transform.position.y + (fHeight / 2);
+                m_vLimitAreaCenter = new Vector3(fCenterX, fCenterY, 0);
+            }
+
+            Gizmos.DrawWireCube(m_vLimitAreaCenter, m_vLimitArea);
+        }
+    }
+
+    private Vector3 m_vLimitAreaCenter;
+    private Vector3 m_vLimitArea;
+
     [SerializeField, Rename("Camera Transform")]
     protected Transform m_vCameraTransform = null;
 
